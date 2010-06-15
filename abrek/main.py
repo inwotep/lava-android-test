e9f02fb1cf89049e8dc589c599adf26285b25574
Paul Larson <paul.larson@canonical.com>
2010-06-15 08:43:10 -0500

    handle missing or unknown commands

--- abrek/main.py
+++ abrek/main.py
@@ -4,7 +4,12 @@ import abrek.commands
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
+        if not argv:
+            argv = ['help']
         cmd = argv.pop(0)
         cmd_func = abrek.commands.get_command(cmd)
+        if not cmd_func:
+            print "command '%s' not found" % cmd
+            return 1
         run = cmd_func.run
         run(argv)
