47968872595e0bef49fe66a069710e1ec0b21305
Paul Larson <paul.larson@canonical.com>
2010-06-16 13:25:37 -0500

    Convert to simple config class to manage data and config dirs.  This may eventually be used for other general configuration related tasks.

--- abrek/config.py
+++ abrek/config.py
@@ -1,26 +1,16 @@
 import os
 
-def config_dir():
-    basedir = os.path.expanduser("~")
-    configdir = os.path.join(basedir, '.abrek')
-    if not os.path.exists(configdir):
-        os.makedirs(configdir)
-    return configdir
+class AbrekConfig(object):
+    def __init__(self):
+        home = os.environ.get('HOME', '/')
+        baseconfig = os.environ.get('XDG_CONFIG_HOME',
+                     os.path.join(home, '.config'))
+        basedata = os.environ.get('XDG_DATA_HOME',
+                     os.path.join(home, '.local', 'share'))
+        basecache = os.environ.get('XDG_CACHE_HOME',
+                     os.path.join(home, '.cache'))
+        self.configdir = os.path.join(baseconfig, 'abrek')
+        self.installdir = os.path.join(basedata, 'abrek', 'installed-tests')
+        self.downloaddir = os.path.join(basecache, 'abrek', 'download')
+        self.resultsdir = os.path.join(basedata, 'abrek', 'results')
 
-def config_installed_dir():
-    installdir = os.path.join(config_dir(), 'installed-tests')
-    if not os.path.exists(installdir):
-        os.makedirs(installdir)
-    return installdir
-
-def config_download_dir():
-    downloaddir = os.path.join(config_dir(), 'download')
-    if not os.path.exists(downloaddir):
-        os.makedirs(downloaddir)
-    return downloaddir
-
-def config_results_dir():
-    resultsdir = os.path.join(config_dir(), 'results')
-    if not os.path.exists(resultsdir):
-        os.makedirs(resultsdir)
-    return resultsdir
