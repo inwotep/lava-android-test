969e03c9df2cb927a708b3711083daa7375d0a12
Paul Larson <paul.larson@canonical.com>
2010-06-24 14:52:29 -0500

    improved geturl to handle bad urls and failed downloads better

--- abrek/utils.py
+++ abrek/utils.py
@@ -1,5 +1,5 @@
 import os
-import urllib
+import urllib2
 import urlparse
 
 def geturl(url, path=""):
@@ -7,7 +7,18 @@ def geturl(url, path=""):
     filename = os.path.basename(urlpath)
     if path:
         filename = os.path.join(path,filename)
-    urllib.urlretrieve(url, filename)
+    fd = open(filename, "w")
+    try:
+        response = urllib2.urlopen(url)
+        with open(filename, 'wb') as fd:
+            while True:
+                data = response.read(0x10000)
+                if not data:
+                    break
+                fd.write(data)
+        response.close()
+    except:
+        raise RuntimeError, "Could not retrieve %s" % url
     return filename
 
 def write_file(data,path):
