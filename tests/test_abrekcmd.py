67611d8c578d8b6b3a3ac48e254233fd60c0db89
Paul Larson <paul.larson@canonical.com>
2010-07-02 17:22:03 -0500

    Added more tests for command related things

--- tests/test_abrekcmd.py
+++ tests/test_abrekcmd.py
@@ -1,8 +1,26 @@
 import unittest
-from abrek.command import AbrekCmd
+from abrek.command import AbrekCmd, get_command, get_all_cmds
 
 class testAbrekCmd(unittest.TestCase):
     def test_empty_run(self):
         cmd = AbrekCmd()
         self.assertRaises(NotImplementedError, cmd.run, ['foo'])
 
+    def test_name(self):
+        class cmd_test_name(AbrekCmd):
+            pass
+        cmd = cmd_test_name()
+        self.assertEqual(cmd.name(),"test-name")
+
+    def test_help(self):
+        cmd = AbrekCmd()
+        print cmd.help()
+        self.assertTrue(cmd.help())
+
+    def test_get_command(self):
+        cmd = get_command("install")
+        self.assertTrue(isinstance(cmd, AbrekCmd))
+
+    def test_get_all_cmds(self):
+        cmds = get_all_cmds()
+        self.assertTrue("install" in cmds)
