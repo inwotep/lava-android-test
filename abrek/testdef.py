145c65beb98f05a9e0ce4d15676b2c0388a42254
Paul Larson <paul.larson@canonical.com>
2010-07-22 16:45:52 +0200

    Fix bad fixresults call

--- abrek/testdef.py
+++ abrek/testdef.py
@@ -237,7 +237,7 @@ class AbrekTestParser(object):
                 if match:
                     self.results['testlist'].append(match.groupdict())
         if self.fixupdict:
-            self.fixresults()
+            self.fixresults(self.fixupdict)
         if self.appendall:
             self.appendtoall(self.appendall)
 
