78155d3438a0844e40732b095d7b769e87b4348b
Paul Larson <paul.larson@canonical.com>
2010-08-06 13:18:10 -0500

    Add a read_file() utility function and a fake_file() for testing

--- abrek/utils.py
+++ abrek/utils.py
@@ -3,6 +3,8 @@ import os
 import urllib2
 import urlparse
 
+_fake_files = None
+
 def geturl(url, path=""):
     urlpath = urlparse.urlsplit(url).path
     filename = os.path.basename(urlpath)
@@ -19,7 +21,24 @@ def geturl(url, path=""):
         raise RuntimeError("Could not retrieve %s" % url)
     return filename
 
-def write_file(data,path):
+def write_file(data, path):
     with open(path, "w") as fd:
         fd.write(data)
 
+def read_file(path):
+    global _fake_files
+    if _fake_files is not None:
+        if path in _fake_files:
+            return _fake_files[path]
+    with open(path) as fd:
+        data = fd.read()
+    return data
+
+def fake_file(path, data):
+    """
+    Set up a fake file to be read with read_file() in testing
+    """
+    global _fake_files
+    if _fake_files is None:
+        _fake_files = {}
+    _fake_files[path] = data
