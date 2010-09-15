b7ca4aac6046e1239ec58826c5d9a4638bf7f099
Paul Larson <paul.larson@canonical.com>
2010-09-15 15:32:29 -0500

    Give a more useful error when calling a subcommand that doesn't exist

--- abrek/main.py
+++ abrek/main.py
@@ -17,15 +17,19 @@ import sys
 import abrek.command
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-        if not argv:
-            argv = ['help']
-        cmd = argv.pop(0)
-        cmd_func = abrek.command.get_command(cmd)
-        if not cmd_func:
-            print "command '%s' not found" % cmd
-            return 1
-        main = cmd_func.main
+def main(argv):
+    origargv = argv
+    argv = argv[1:]
+    if not argv:
+        argv = ['help']
+    cmd = argv.pop(0)
+    cmd_func = abrek.command.get_command(cmd)
+    if not cmd_func:
+        print "command '%s' not found" % cmd
+        return 1
+    main = cmd_func.main
+    try:
         main(argv)
+    except NotImplementedError:
+        print >>sys.stderr, "Unknown usage '%s'" % " ".join(origargv)
+        print >>sys.stderr, "Use 'abrek help [cmd]' for help"
