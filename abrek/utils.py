6b9259b060bce64f9582a9cecd0999e211f2c6cb
Paul Larson <paul.larson@canonical.com>
2010-09-17 23:21:56 -0500

    Extend fake_files interface to allow it to point not only at fake data, but a fake path as well.  This is especially useful when trying to simulate a situation where the file does not exist.  Also add a method to clear the list of fake data/paths.

--- abrek/utils.py
+++ abrek/utils.py
@@ -19,6 +19,7 @@ import urllib2
 import urlparse
 
 _fake_files = None
+_fake_paths = None
 
 def geturl(url, path=""):
     urlpath = urlparse.urlsplit(url).path
@@ -42,18 +43,37 @@ def write_file(data, path):
 
 def read_file(path):
     global _fake_files
+    global _fake_paths
     if _fake_files is not None:
         if path in _fake_files:
             return _fake_files[path]
+    if _fake_paths is not None:
+        if path in _fake_paths:
+            path = _fake_paths[path]
     with open(path) as fd:
         data = fd.read()
     return data
 
-def fake_file(path, data):
+def fake_file(path, data=None, newpath=None):
     """
     Set up a fake file to be read with read_file() in testing
+    If data is specified, the string passed as data will be returned instead
+    if newpath is specified, the file attempted to be read will be replaced
+    by newfile
     """
     global _fake_files
-    if _fake_files is None:
-        _fake_files = {}
-    _fake_files[path] = data
+    global _fake_paths
+    if data is not None:
+        if _fake_files is None:
+            _fake_files = {}
+        _fake_files[path] = data
+    if newpath is not None:
+        if _fake_paths is None:
+            _fake_paths = {}
+        _fake_paths[path] = newpath
+
+def clear_fakes():
+    global _fake_files
+    global _fake_paths
+    _fake_files = {}
+    _fake_paths = {}
