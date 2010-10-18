e9a8203354c1e02c6caa88dadeee39ea2fb3af30
Paul Larson <paul.larson@canonical.com>
2010-10-18 11:03:50 -0500

    add license header

--- abrek/test_definitions/x11perf.py
+++ abrek/test_definitions/x11perf.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2010 Linaro
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import abrek.testdef
 
 x11perf_options = "-repeat 3"
@@ -31,7 +47,7 @@ PATTERN = "trep @.*\(\W*(?P<measurement>\d+.\d+)/sec\):\W+(?P<test_case_id>.+)"
 inst = abrek.testdef.AbrekTestInstaller(deps=["x11-apps"])
 run = abrek.testdef.AbrekTestRunner(RUNSTEPS)
 parse = abrek.testdef.AbrekTestParser(PATTERN,
-                                      appendall={'units':'reps/s', 
+                                      appendall={'units':'reps/s',
                                                  'result':'pass'})
 
 testobj = abrek.testdef.AbrekTest(testname="x11perf", installer=inst,
