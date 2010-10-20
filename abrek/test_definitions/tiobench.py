04bce102ebd0fae88963fc24fcd8ea4c68086d88
Paul Larson <paul.larson@canonical.com>
2010-10-20 16:36:44 -0500

    minor cleanups

--- abrek/test_definitions/tiobench.py
+++ abrek/test_definitions/tiobench.py
@@ -16,8 +16,8 @@
 """
    This script automates the installation, execution, and
    results parsing for the tiobench test suite.
-   tiobench is a multi-threaded I/O benchmark. It is used to measure 
-   file system performance in four basic operations: sequential read, 
+   tiobench is a multi-threaded I/O benchmark. It is used to measure
+   file system performance in four basic operations: sequential read,
    random read, sequential write, and random write.
 """
 import re
@@ -26,7 +26,7 @@ import abrek.testdef
 VERSION="0.3.3"
 URL="http://prdownloads.sourceforge.net/tiobench/tiobench-%s.tar.gz" %(VERSION)
 MD5="bf485bf820e693c79e6bd2a38702a128"
-INSTALLSTEPS = ['tar -zxvf tiobench-%s.tar.gz' % VERSION, 
+INSTALLSTEPS = ['tar -zxvf tiobench-%s.tar.gz' % VERSION,
                 'cd tiobench-%s && make' % VERSION]
 RUNSTEPS = ["cd tiobench-%s && "\
             "./tiobench.pl --block=4096 --block=8192 --threads=2 "\
@@ -54,16 +54,17 @@ class TIObenchTestParser(abrek.testdef.AbrekTestParser):
                     results = match2.groupdict()
                     blks_size = results.pop('blks_size')
                     filesize = results.pop('file_size')
-                    results['test_case_id'] = ('%s_%sMBfilesize_%sbytesblksize') \
-                                              % (tc_id, filesize, blks_size)
+                    results['test_case_id'] = (
+                        '%s_%sMBfilesize_%sbytesblksize') % (
+                            tc_id, filesize, blks_size)
                     self.results['test_results'].append(results)
 
         if self.appendall:
             self.appendtoall(self.appendall)
 
-tiobench_inst = abrek.testdef.AbrekTestInstaller(INSTALLSTEPS, url=URL, md5=MD5)
+tiobench_inst = abrek.testdef.AbrekTestInstaller(INSTALLSTEPS, url=URL,
+    md5=MD5)
 tiobench_run = abrek.testdef.AbrekTestRunner(RUNSTEPS)
 parse = TIObenchTestParser(appendall={'units':'MB/s', 'result':'pass'})
 testobj = abrek.testdef.AbrekTest(testname="tiobench", version=VERSION,
-                                  installer=tiobench_inst, runner=tiobench_run,
-                                  parser=parse)
+    installer=tiobench_inst, runner=tiobench_run, parser=parse)
