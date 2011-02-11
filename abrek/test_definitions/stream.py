353b4d1aae941161e8fb3dd46b1329fe5614d74e
Paul Larson <paul.larson@canonical.com>
2011-02-11 13:54:54 -0600

    Stream has mucked with things, doesn't seem to have a reliable download from upstream.  Ignore md5 for now.

--- abrek/test_definitions/stream.py
+++ abrek/test_definitions/stream.py
@@ -16,12 +16,11 @@
 import abrek.testdef
 
 URL="http://www.cs.virginia.edu/stream/FTP/Code/stream.c"
-MD5="b6cd43b848e0d8b0824703369392f3c5"
 INSTALLSTEPS = ['cc stream.c -O2 -fopenmp -o stream']
 RUNSTEPS = ['./stream']
 PATTERN = "^(?P<test_case_id>\w+):\W+(?P<measurement>\d+\.\d+)"
 
-streaminst = abrek.testdef.AbrekTestInstaller(INSTALLSTEPS, url=URL, md5=MD5)
+streaminst = abrek.testdef.AbrekTestInstaller(INSTALLSTEPS, url=URL)
 streamrun = abrek.testdef.AbrekTestRunner(RUNSTEPS)
 streamparser = abrek.testdef.AbrekTestParser(PATTERN,
                appendall={'units':'MB/s', 'result':'pass'})
