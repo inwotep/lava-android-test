2209fa319491badef0f6fdd9fc511a43efaf5c94
Le Chi Thu le.chi.thu@linaro.org <le.chi.thu@linaro.org>
2012-01-26 22:57:36 +0100

    Added gatortest and fixed the bug 922324

---
+++ lava_android_test/test_definitions/gatortest/daemoncheck.py
@@ -0,0 +1,22 @@
+import sys
+import re
+import os
+from commands import getstatusoutput
+
+if len(sys.argv) == 1:
+    adbcmd = 'adb'
+else:
+    adbcmd = 'adb -s %s' % (sys.argv[1])
+
+cmd = '%s shell ps' % (adbcmd)
+rc, output = getstatusoutput(cmd)
+if rc != 0:
+    print 'Failed to run command %s : %s' % (cmd, output)
+    sys.exit(1)
+
+# parse output
+
+if output.find("gator") != -1:
+    print "gator_daemon_check=pass"
+else:
+    print "gator_daemon_check=fail"
---
+++ lava_android_test/test_definitions/gatortest/modulecheck.py
@@ -0,0 +1,22 @@
+import sys
+import re
+import os
+from commands import getstatusoutput
+
+if len(sys.argv) == 1:
+    adbcmd = 'adb'
+else:
+    adbcmd = 'adb -s %s' % (sys.argv[1])
+
+cmd = '%s shell lsmod' % (adbcmd)
+rc, output = getstatusoutput(cmd)
+if rc != 0:
+    print 'Failed to run command %s : %s' % (cmd, output)
+    sys.exit(1)
+
+# parse output
+
+if output.find("gator") != -1:
+    print "gator_module_check=pass"
+else:
+    print "gator_module_check=fail"
