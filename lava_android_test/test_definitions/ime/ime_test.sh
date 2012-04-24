fb0e7e86ae2e8a5c66988604707b30676cde7f31
Ke Jun ZHOU <dp583@stebjsxu0119>
2012-04-24 18:52:51 +0800

    for inputmethod service

---
+++ lava_android_test/test_definitions/ime/ime_test.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+test_func(){
+    if [ ! -f /system/bin/ime ]; then
+         echo "ime=unexist"
+         exit
+    fi  
+
+    /system/bin/ime list -a
+    echo "ime = pass"
+}
+
+test_func
