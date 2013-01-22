495ffa4782b4041c9413f382c89db9e088df63b6
Yongqin Liu <yongqin.liu@linaro.org>
2013-01-22 14:23:38 +0800

    exclue the shells/commands/hostshells test from the list-tests command
    these three test shells/commands/hostshells are not the real test,
    and so that they can not be install and run, so here add the fix to
    exclude them from the list-tests sub command

--- lava_android_test/provider.py
+++ lava_android_test/provider.py
@@ -113,7 +113,21 @@ class TestProvider(object):
 class CommonTestProvider(TestProvider):
 
     def list_test(self):
-        return self.list_mod(test_definitions.__path__)
+        providers = self.get_test_provider_list()
+        avoid_dirs = []
+        for provider in providers:
+            test_prefix = provider.test_prefix
+            if test_prefix:
+                avoid_dirs.append('%ss' % test_prefix)
+        test_list = self.list_mod(test_definitions.__path__)
+        if not avoid_dirs:
+            return test_list
+        else:
+            ret_list = []
+            for test_id in test_list:
+                if not test_id in avoid_dirs:
+                    ret_list.append(test_id)
+            return ret_list
 
     def load_test(self, test_name=None, serial=None):
         importpath = "lava_android_test.test_definitions.%s" % test_name
@@ -231,7 +245,7 @@ class ShellTestProvider(TestProvider):
                                 steps_adb_pre=INSTALL_STEPS_ADB_PRE),
                     runner=testdef.AndroidTestRunner(
                                     adbshell_steps=ADB_SHELL_STEPS),
-                    parser=testdef.AndroidSimpleTestParser(PATTERN),
+                    parser=testdef.AndroidSimpleTestParser(),
                     adb=ADB(serial))
         return testobj
 
@@ -273,4 +287,3 @@ class HostShellTestProvider(TestProvider):
                     parser=testdef.AndroidSimpleTestParser(),
                     adb=ADB(serial))
         return testobj
-
