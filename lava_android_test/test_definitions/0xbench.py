ea8b249d3961580d8fe6d67ce4f8f85d682b2c29
Yongqin Liu <yongqin.liu@linaro.org>
2013-01-22 20:05:33 +0800

    update 0xbench to accept specified option from android-build page
    1. test to be run can be specified as "--ez <test> true", where "<test>"
       can be math/2d/3d/vm/native
    2. the time for waiting finish can be specified from android-build
       as option like "-t 3600", but this must be specified with the test to be run,
       otherwise there will be no test to run automatically

--- lava_android_test/test_definitions/0xbench.py
+++ lava_android_test/test_definitions/0xbench.py
@@ -34,6 +34,9 @@ import lava_android_test.testdef
 
 curdir = os.path.realpath(os.path.dirname(__file__))
 
+# "-t timeout" must be specified with at least one test
+# can not only specified the "-t timeout" as option
+DEFAULT_OPTIONS = '--ez math true --ez 2d true --ez 3d true --ez vm true'
 INSTALL_STEPS_HOST_POST = [
         ("python %s/android-0xbenchmark/android_0xbenchmark_modify_path.py"
          " $(SERIAL)") % curdir]
@@ -43,8 +46,8 @@ RUN_STEPS_HOST_PRE = [
          " $(SERIAL)") % curdir]
 RUN_STEPS_ADB_SHELL = ['logcat -c',
         ("am start -n org.zeroxlab.zeroxbenchmark/"
-         "org.zeroxlab.zeroxbenchmark.Benchmark --ez math true"
-         " --ez 2d true --ez 3d true --ez vm true --ez autorun true")]
+         "org.zeroxlab.zeroxbenchmark.Benchmark $(OPTIONS) "
+         "--ez autorun true")]
 RUN_STEPS_HOST_POST = [
         'python %s/android-0xbenchmark/android_0xbenchmark_wait.py $(SERIAL)' %
          curdir]
@@ -82,4 +85,5 @@ run = lava_android_test.testdef.AndroidTestRunner(
 parser = ZeroXBenchmarkTestParser()
 testobj = lava_android_test.testdef.AndroidTest(testname="0xbench",
             installer=inst, runner=run, parser=parser,
-             org_ouput_file=os.path.join(save_dir, '0xBenchmark.bundle'))
+            org_ouput_file=os.path.join(save_dir, '0xBenchmark.bundle'),
+            default_options=DEFAULT_OPTIONS)
