7dde2a5096a138f3ead51134794294cd61291510
Amit Pundir <amit.pundir@linaro.org>
2014-07-18 10:07:21 +0530

    monkey_long_run: use new blacklist for juno
    Change-Id: I385024cc1947a04cdd31ae2d63c3dc22477f9084
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Reviewed-on: https://review.linaro.org/2521
    Reviewed-by: YongQin Liu <yongqin.liu@linaro.org>
    Reviewed-by: Dave Pigott <dave.pigott@linaro.org>

--- lava_android_test/test_definitions/monkey_long_run/monkey_long_run.sh
+++ lava_android_test/test_definitions/monkey_long_run/monkey_long_run.sh
@@ -2,6 +2,8 @@
 #monkey_cmd="monkey -s 1 --pct-touch 10 --pct-motion 20 --pct-nav 20 --pct-majornav 30 --pct-appswitch 20 --throttle 500 2147483647"
 if [ $1 == 'juice' ]; then
 monkey_cmd="monkey -s 1 --pct-touch 10 --pct-motion 20 --pct-nav 20 --pct-majornav 30 --pct-appswitch 20 --throttle 500 --pkg-blacklist-file $2 30000"
+elif [ $1 == 'juno' ]; then
+monkey_cmd="monkey -s 1 --pct-touch 10 --pct-motion 20 --pct-nav 20 --pct-majornav 30 --pct-appswitch 20 --throttle 500 --pkg-blacklist-file /data/juno_monkey_blacklist 30000"
 else
 monkey_cmd="monkey -s 1 --pct-touch 10 --pct-motion 20 --pct-nav 20 --pct-majornav 30 --pct-appswitch 20 --throttle 500 --pkg-blacklist-file $3 25000"
 fi
